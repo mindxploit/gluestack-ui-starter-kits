1a7612fc9c224f88b2ba2d3246f3f4d1920c7f5d
mindxploit <bobbonicola@gmail.com>
2025-08-14 16:27:42 +0200

    keyboardavoidingview

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,7 +1,7 @@
 import { Image } from "@/components/ui/image";
 import { Input, InputSlot } from "@/components/ui/input";
 import { InputField } from "@/components/ui/input";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import { BlurView } from "expo-blur";
 import { VStack } from "@/components/ui/vstack";
 import { Badge, BadgeText } from "@/components/ui/badge";
@@ -23,35 +23,40 @@ export default function Tab() {
     <View style={styles.container}>
       <Image alt="jesus" source={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa8%2Fc2%2Ffc%2Fa8c2fc1833d28007d3507652a70ea78c.jpg&f=1&nofb=1&ipt=574b6a1bcf2d54761387582b704a975c861c7cb5a4e11c50707079cc11ed5b90"} className="w-full h-full absolute object-cover" />
 
-      <VStack space="md" className="w-full p-4" style={{ marginBottom: tabBarHeight }}>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.scrollContainer}
-        >
-          {suggestions.map((suggestion, index) => (
-            <BlurView key={index} intensity={90} style={styles.blurContainer}>
-              <Badge action="neutral" variant="outline" size="lg" className="bg-transparent rounded-full">
-                <BadgeText size="sm">
-                  <Text>{suggestion}</Text>
-                </BadgeText>
-              </Badge>
-            </BlurView>
-          ))}
-        </ScrollView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        // offset based on the tabs margin
+        keyboardVerticalOffset={-tabBarHeight}>
+        <VStack space="md" className="w-full p-4" style={{ marginBottom: tabBarHeight }}>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.scrollContainer}
+          >
+            {suggestions.map((suggestion, index) => (
+              <BlurView key={index} intensity={90} style={styles.blurContainer}>
+                <Badge action="neutral" variant="outline" size="lg" className="bg-transparent rounded-full">
+                  <BadgeText size="sm">
+                    <Text>{suggestion}</Text>
+                  </BadgeText>
+                </Badge>
+              </BlurView>
+            ))}
+          </ScrollView>
 
-        <BlurView intensity={90} style={styles.blurContainer}>
-          <Input variant="rounded" size="xl" isDisabled={false} isInvalid={false} isReadOnly={false} className="border-background-300">
-            <InputField
-              placeholder='Ask me anything...'
-            />
-            <InputSlot className="px-3">
-              <MaterialIcons name="send" size={24} className="text-background-300" />
-            </InputSlot>
-          </Input>
-        </BlurView>
-      </VStack>
-    </View>
+          <BlurView intensity={90} style={styles.blurContainer}>
+            <Input variant="rounded" size="xl" isDisabled={false} isInvalid={false} isReadOnly={false} className="border-background-300">
+              <InputField
+                placeholder='Ask me anything...'
+              />
+              <InputSlot className="px-3">
+                <MaterialIcons name="send" size={24} className="text-background-300" />
+              </InputSlot>
+            </Input>
+          </BlurView>
+        </VStack>
+      </KeyboardAvoidingView>
+    </View >
   );
 }
 
