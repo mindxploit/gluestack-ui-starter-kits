195a69c772ca1aac5a749c88ced14e35d98dd818
mindxploit <bobbonicola@gmail.com>
2025-08-15 11:20:05 +0200

    styling refactor flex chat

--- components/AvatarChat/index.tsx
+++ components/AvatarChat/index.tsx
@@ -22,6 +22,7 @@ import Animated, {
 import { Button, ButtonIcon, ButtonText } from "../ui/button";
 import { Directions, Gesture, GestureDetector, GestureEvent, PanGestureHandler, State } from "react-native-gesture-handler";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 
 interface AvatarChatProps {
@@ -36,6 +37,30 @@ const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 export const AvatarChat = ({ avatar, suggestions }: AvatarChatProps) => {
   const anim = useSharedValue(1);
+  const dragY = useSharedValue(0);
+
+  const tap = Gesture.Fling()
+    .onBegin(() => {
+      anim.value === 0 && toggleChat();
+    }).runOnJS(true);
+
+  const dragGesture = Gesture.Pan()
+    .onUpdate((event) => {
+      if (event.translationY > 0) {
+        dragY.value = event.translationY;
+        if (event.translationY > 200) {
+          anim.value = 0
+        }
+      }
+    }).runOnJS(true)
+    .onEnd((event) => {
+      // If dragged down far enough, toggle chat
+      if (event.translationY > 100) {
+        anim.value = 0
+      }
+      // Reset drag position
+      dragY.value = withTiming(0);
+    }).runOnJS(true);
 
   const toggleChat = () => {
     anim.value = anim.value === 0 ? 1 : 0;
@@ -46,11 +71,11 @@ export const AvatarChat = ({ avatar, suggestions }: AvatarChatProps) => {
       easing: Easing.inOut(Easing.quad),
       reduceMotion: ReduceMotion.System,
     }
-    const width = withTiming(anim.value === 0 ? 80 : SCREEN_WIDTH, config);
-    const height = withTiming(anim.value === 0 ? 80 : SCREEN_HEIGHT, config);
+    const width = withTiming(anim.value === 0 ? 100 : SCREEN_WIDTH - (dragY.value * 0.6), config);
+    const height = withTiming(anim.value === 0 ? 120 : SCREEN_HEIGHT - dragY.value, config);
     const bottom = withTiming(anim.value === 0 ? 100 : 0, config);
     const right = withTiming(anim.value === 0 ? 0 : 0, config);
-    const radius = withTiming(anim.value === 0 ? 20 : 0, config);
+    const radius = withTiming(anim.value === 0 ? 20 : 20, config);
 
     return {
       width,
@@ -68,46 +93,45 @@ export const AvatarChat = ({ avatar, suggestions }: AvatarChatProps) => {
     };
   });
 
-
-  const tap = Gesture.Fling()
-    .onBegin(() => {
-      anim.value === 0 && toggleChat();
-    }).runOnJS(true);
-
   const tabBarHeight = useBottomTabBarHeight();
+  const insets = useSafeAreaInsets();
+
+  const heightWithoutTabBar = SCREEN_HEIGHT - tabBarHeight
 
   return (
     <View style={styles.container}>
-      <GestureDetector gesture={tap}>
+      <GestureDetector gesture={Gesture.Exclusive(tap, dragGesture)}>
         <Animated.View style={[styles.chatContainer, chatStyle]}>
           <Image alt={avatar.name} source={avatar.video} className="w-full h-full absolute object-cover" />
 
-          <Animated.View className="absolute top-16 left-5" style={hiddenStyle}>
-            <BlurView intensity={90} style={styles.blurContainer}>
-              <Badge action="neutral" variant="solid" size="lg" className="rounded-full">
-                <BadgeText size="lg" className="font-bold">
-                  {avatar.name}
-                </BadgeText>
-              </Badge>
-            </BlurView>
-          </Animated.View>
-
-          <Animated.View className="absolute top-16 right-5" style={hiddenStyle}>
-            <Pressable onPress={toggleChat}>
-              <BlurView intensity={30} style={styles.blurContainer}>
-                <Button variant="solid" size="xs" className="rounded-full bg-transparent opacity-70" onPress={toggleChat}>
-                  <FontAwesome name="chevron-down" size={16} color="white" />
-                </Button>
-              </BlurView>
-            </Pressable>
-          </Animated.View>
-
           <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            // offset based on the tabs margin
+            // offset based on the bottom tabs margin
             keyboardVerticalOffset={-tabBarHeight}>
+
             <Animated.View style={hiddenStyle}>
-              <VStack space="md" className="w-full p-4" style={{ marginBottom: tabBarHeight }}>
+              <VStack space="md" className="w-full px-4 pb-4" style={{ marginBottom: tabBarHeight, justifyContent: 'center', height: heightWithoutTabBar }}>
+                <HStack className="justify-between">
+                  <Animated.View style={[hiddenStyle, { marginTop: insets.top }]}>
+                    <BlurView intensity={90} style={styles.blurContainer}>
+                      <Badge action="neutral" variant="solid" size="lg" className="rounded-full">
+                        <BadgeText size="lg" className="font-bold">
+                          {avatar.name}
+                        </BadgeText>
+                      </Badge>
+                    </BlurView>
+                  </Animated.View>
+
+                  <Animated.View style={[hiddenStyle, { marginTop: insets.top }]}>
+                    <Pressable onPress={toggleChat}>
+                      <BlurView intensity={30} style={styles.blurContainer}>
+                        <Button variant="solid" size="xs" className="rounded-full bg-transparent opacity-70" onPress={toggleChat}>
+                          <FontAwesome name="chevron-down" size={16} color="white" />
+                        </Button>
+                      </BlurView>
+                    </Pressable>
+                  </Animated.View>
+                </HStack>
                 <ScrollView
                   horizontal={true}
                   showsHorizontalScrollIndicator={false}
@@ -115,7 +139,7 @@ export const AvatarChat = ({ avatar, suggestions }: AvatarChatProps) => {
                 >
                   {suggestions.map((suggestion, index) => (
                     // @ts-ignore
-                    <BlurView intensity={90} style={styles.blurContainer}>
+                    <BlurView intensity={90} style={[styles.blurContainer, { alignSelf: "flex-end" }]}>
                       <Badge action="neutral" variant="outline" size="lg" className="bg-transparent rounded-full">
                         <BadgeText size="sm">
                           <Text>{suggestion}</Text>
@@ -151,7 +175,7 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "flex-end",
     width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
+    height: SCREEN_HEIGHT
   },
   blurContainer: {
     borderRadius: 100,
@@ -161,12 +185,6 @@ const styles = StyleSheet.create({
     gap: 5,
   },
   chatContainer: {
-    position: 'absolute',
     overflow: 'hidden',
-    borderRadius: 100,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
   },
 });
